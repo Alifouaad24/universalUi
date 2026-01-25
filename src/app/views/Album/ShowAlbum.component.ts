2737e8d683afe074f17840f29d058435b96aabe7
AliFouaad24 <alifouaad24@gmail.com>
2026-01-25 22:59:35 +0300

    resize album imgs

--- src/app/views/Album/ShowAlbum.component.ts
+++ src/app/views/Album/ShowAlbum.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
-import { BadgeComponent, ButtonDirective, FormModule, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ProgressBarComponent, ProgressComponent, TableDirective, ToastBodyComponent, ToastCloseDirective, ToastComponent, ToastModule } from '@coreui/angular';
+import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, signal } from '@angular/core';
+import { BadgeComponent, ButtonDirective, ButtonModule, CardModule, FormModule, GridModule, ModalBodyComponent, ModalComponent, ModalFooterComponent, ModalHeaderComponent, ModalModule, ProgressBarComponent, ProgressComponent, TableDirective, ToastBodyComponent, ToastCloseDirective, ToastComponent, ToastModule } from '@coreui/angular';
 import { IconComponent } from '@coreui/icons-angular';
 
 import {
@@ -14,18 +14,24 @@ import { HttpConnectService } from '../../Services/http-connect.service';
 import { AlbumModel } from '../../Models/Album';
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: 'ShowAlbum.component.html',
   imports: [CardComponent, CardHeaderComponent, ToastComponent, ToastBodyComponent,
-    ToastCloseDirective, ToastModule, ProgressComponent,
+    ToastCloseDirective, ToastModule, ProgressComponent, CardModule,
+    ButtonModule,
+    ModalModule,
+    GridModule, ProgressBarComponent,
     ModalComponent, ModalHeaderComponent, ModalBodyComponent, ModalFooterComponent,
     CardBodyComponent, RowComponent, RouterLink, RouterOutlet,
-    BadgeComponent, CommonModule, FormModule, IconComponent, TableDirective, ButtonDirective]
+    BadgeComponent, CommonModule, FormModule, IconComponent, TableDirective]
 })
 export class ShowAlbumComponent implements OnInit {
 
   allAlbums: AlbumModel[] = []
   message?: string
   isLoading: boolean = false;
+  modalVisible = false;
+  currentIndex = 0;
   showDeleteModal: boolean = false;
   ///// for toastr ////////
   position = 'top-end';
@@ -55,6 +61,46 @@ export class ShowAlbumComponent implements OnInit {
         this.message = 'Error loading data';
         this.cdr.detectChanges();
       })
+  }
+
+  openModal(index: number) {
+    this.currentIndex = index;
+    this.modalVisible = true;
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+  }
+
+  prevImage() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  nextImage() {
+    if (this.currentIndex < this.allAlbums.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  deleteImage() {
+    if (!confirm('Are you sure you want to delete this image?')) return;
+
+    const deletedImage = this.allAlbums[this.currentIndex];
+    this.http.deleteData(`ImageUploader/${deletedImage.userImagesId}`).subscribe((res) => {
+      this.toastMessage.set('Image deleted successfully');
+      this.toastVisible.set(true);
+      this.allAlbums.splice(this.currentIndex, 1);
+      if (this.currentIndex >= this.allAlbums.length) {
+        this.currentIndex = this.allAlbums.length - 1;
+      }
+      if (this.allAlbums.length === 0) {
+        this.closeModal();
+      }
+      this.cdr.detectChanges();
+    })
+
   }
 
   // confirmDelete(type: AddressModel) {
